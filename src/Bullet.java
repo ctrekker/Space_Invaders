3795b81936f46f528e2d4767cda4d4e5befa2859
Connor Burns <ctrekker4@gmail.com>
2018-02-26 19:25:34 -0700

    Bullet Data-layer Completion

--- src/Bullet.java
+++ src/Bullet.java
@@ -2,17 +2,39 @@ import java.awt.Color;
 
 
 public class Bullet {
+	private static final int DEFAULT_X=0;
+	private static final int DEFAULT_Y=0;
+	private static final int DEFAULT_WIDTH=10;
+	private static final int DEFAULT_HEIGHT=10;
+	private static final Vector2D DEFAULT_DIRECTION=new Vector2D();
+	private static final Color DEFAULT_COLOR=Color.WHITE;
+
 	private int x;
 	private int y;
 	private int width;
 	private int height;
     private Vector2D direction;
 	private Color color;
-	
+
+	public Bullet() {
+		this(DEFAULT_X, DEFAULT_Y, DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_DIRECTION, DEFAULT_COLOR);
+	}
 	public Bullet(int x, int y) {
+		this(x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_DIRECTION, DEFAULT_COLOR);
+	}
+	public Bullet(int x, int y, int width, int height) {
+		this(x, y, width, height, DEFAULT_DIRECTION, DEFAULT_COLOR);
+	}
+	public Bullet(int x, int y, int width, int height, Vector2D direction) {
+		this(x, y, width, height, direction, DEFAULT_COLOR);
+	}
+	public Bullet(int x, int y, int width, int height, Vector2D direction, Color color) {
 		this.x=x;
 		this.y=y;
-		this.direction=new Vector2D();
+		this.width=width;
+		this.height=height;
+		this.direction=direction;
+		this.color=color;
 	}
 
     public int getX() {
