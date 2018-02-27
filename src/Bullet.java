b1384e6d7723feec514e124fb88e51954475724c
Connor Burns <ctrekker4@gmail.com>
2018-02-26 18:59:25 -0700

    Bullet Organization

--- src/Bullet.java
+++ src/Bullet.java
@@ -5,45 +5,50 @@ public class Bullet {
 	
 	private int x;
 	private int y;
-	private int speed;
 	private int width;
 	private int height;
+    private int speed;
 	private Color color;
 	
-	public Bullet() {
-		
-	}
-	
-	public int getSpeed() {
-		return speed;
+	public Bullet(int x, int y) {
+		this.x=x;
+		this.y=y;
 	}
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
 	public int getWidth() {
 		return width;
 	}
-
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
 	public int getHeight() {
 		return height;
 	}
-
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 	public Color getColor() {
 		return color;
 	}
-
 	public void setColor(Color color) {
 		this.color = color;
 	}
-	
 }
